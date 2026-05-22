4c5a32faeaca5beea33de11d4ab20db60fdb3e52
Álvaro Pérez Amado <56943265+alvperez@users.noreply.github.com>
2026-05-22 10:12:52 -0500

    Update leaderboard CSV URL, form ID, and entry ID

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 const DATA_SRC = 'https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026';
 // EDITA ESTAS 3 COSAS POR FAVOR
 // POR FAVOR
-const LEADERBOARD_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDbPhOej3DnN_bdvrCQ5R0T6HZg6bBaxKdH17J_Pc3oGOkKkd9V83BUDYlBSCevOrqYK2XQuA7ZMCx/pub?gid=1633860364&single=true&output=csv'
-const FORM_ID = '1FAIpQLSdiF0qsK65DcaadNKRzDbue8xtkzAIIev-7yqUqAH3srhEAQg';
-const ENTRY_ID = 'entry.496944209';
+const LEADERBOARD_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRbGUyubWGwHmks1ha9HG7uHGFkbm-eOCUMnx8Gch1Kb7DSGNJjTUWAxLyU9L0unYkDPYSAa7LFoKB0/pub?gid=1275372669&single=true&output=csv'
+const FORM_ID = '1FAIpQLSeASQQL4Uicp790_f_ZHskxkuTn68FaLJ9N-KPWdvsc76D1Uw';
+const ENTRY_ID = 'entry.1687597452';
 // REPITO, POR FAVOR
 
 // Puedes cambiar los valores por tus propias puntuaciones si quieres
