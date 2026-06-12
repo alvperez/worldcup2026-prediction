8dbc668bf9c0d26a1e42c817e1a795828e450e7b
Alvaro Perez <alvaro33perez@gmail.com>
2026-06-12 07:22:17 -0500

    Record South Korea 2-1 Czech Republic (Group A)

--- results-empty.js
+++ results-empty.js
@@ -3,7 +3,8 @@ const RESULTS = {
   thirdPlace: [],
   groupMatches: {
     A: {
-      "Mexico__South Africa": { home: 2, away: 0 }
+      "Mexico__South Africa": { home: 2, away: 0 },
+      "South Korea__Czech Republic": { home: 2, away: 1 }
     }
   },
 
